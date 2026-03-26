78c1da0658859a8622538d1674d2a0d9d1a85fb6
ikiril-parasoft <igor.kirilenko@parasoft.com>
2026-03-26 23:53:04 +0000

    Fix sev2 violations in goo.cpp file

--- goo.cpp
+++ goo.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>  
 #include <string.h>  
+#include <errno.h>
+#include <limits.h>
 
 volatile uint16_t SENSOR_DATA = 0;
 volatile uint16_t MOTOR_SPEED = 0;
@@ -26,6 +28,9 @@ int computeControl(int sensor, int divisor)
     int value = 0;
 
     if (sensor > SENSOR_HIGH_THRESHOLD) {
+        if (divisor == 0) {
+            return DEFAULT_VALUE;
+        }
         return sensor / divisor;
     } else if (sensor > SENSOR_LOW_THRESHOLD) {
         return sensor * MULTIPLIER;
@@ -38,7 +43,14 @@ int computeControl(int sensor, int divisor)
 int processCommand(const char* cmd, const char* arg)
 {
     if (strcmp(cmd, "SET") == 0) {
-        int val = atoi(arg);
+        char* end = NULL;
+        errno = 0;
+        long parsed = strtol(arg, &end, 10);
+        if ((errno != 0) || (end == arg) || (*end != '\0') ||
+            (parsed < 0) || (parsed > (long)UINT16_MAX)) {
+            return -1;
+        }
+        int val = (int)parsed;
         setMotorSpeed((uint16_t)val);
         return val;
     }
